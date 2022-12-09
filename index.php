1ba7c491f6dcb4b76e376a386ebb628ab4f0fb19
Andrew <rostangeles1993@gmail.com>
2022-12-10 00:18:36 +0300

    php

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
 <?php
-
-echo "Hello World"
-
+{
+    echo "Some text";  // Каждая строка в PHP - Это выражение
+    echo "<br>";
 ?>
+<h2>Заголовок второго уровня</h2>
+<?php
+    echo date(DATE_RSS);
+}
+// PSR-2
+?>
+</body>
+</html>
