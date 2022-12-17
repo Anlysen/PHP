e81ec6a435960c5f3603e2ba31ac250b103acba6
Andrew <rostangeles1993@gmail.com>
2022-12-17 19:16:24 +0300

    new

--- work/index.php
+++ work/index.php
@@ -1,25 +1,30 @@
 <?php
-ini_set('error_reporting', E_ALL);
 
-$numb0 = 1234; // десятичное число
-$numb1 = +143; // десятичное число
-$numb2 = -342; // отрицательное число
-$numb3 = 0123; // восьмиричное число
-$numb4 = 0x1A; // шестнадцатиричное число
-// вещественные числа 
-$var = 1.123;
-// логический тип
-// true
-// false
-// строчный тип
-$str = "Hello, world - $numb0";
-$str1 = 'Some text';
-$str2 = 'Some text - \'34\'';
-// неинициализированная переменная 
-$some; // Notice
-// специальный тип null
-$var2 = null;
-unset($str2, $str);
-echo $numb0;
+$var = "some text";
+$num = 123;
+
+// isset() empty()
+
+$status = isset($var, $num, $ppp); // если переменная есть = true
+
+// если нет переменной, пустая строка, "0", null, false = true
+$status2 = empty($num); 
+
+$str = "0";
+// проверка существования переменной
+if (isset($str)){
+    echo "Переменная $str существует" . "<br>";
+}
+if (empty($str)){
+    echo "Переменная $str пустая" . "<br>";
+}
+// Определяет тип переменной
+$flo = 121.35;
+echo is_int($flo);
+
+echo gettype($flo);
+/*
+Типы данных : boolean, integer, double, string, array, object, resource, null, unknown type
+*/
 
 ?>
