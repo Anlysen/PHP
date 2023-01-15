3b5cbf0f5976b3ad5977b60e9ad0678b839712d7
Andrew <rostangeles1993@gmail.com>
2023-01-15 21:50:26 +0300

    д/з

--- homework/homework.php
+++ homework/homework.php
@@ -1,6 +1,30 @@
 <?php
 
-file_put_contents('hello.txt' , 'Hello, world!');
-echo file_get_contents('hello.txt');
+
+// 1- задание
+file_put_contents('hello.txt' , 'Hello, world!'); // Записывает строку
+echo file_get_contents('hello.txt') , "<br>"; // Выводит в окно браузера
+
+file_put_contents('2023-01-15-21-30-40.txt' , '1000');
+echo file_get_contents('2023-01-15-21-30-40.txt') , "<br>";
+
+
+// 2- задание
+$x = 10;
+$y = 5;
+$z = 2;
+
+if($x > $y){
+    echo "Переменная: $x больше переменной: $y";
+}else{
+    echo "Переменная: $x меньше переменной: $y";
+}
+echo "<br>";
+
+
+// 3- задание
+if($x && $y){
+    echo "Одна переменная больше другой";
+}
 
 ?>
